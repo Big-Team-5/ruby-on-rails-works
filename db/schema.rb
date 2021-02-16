d2fdfb05775826a0262fcc79c6b50218b123fa43
ayadisirine <sirine.ayadi93@gmail.com>
2021-02-16 14:25:27 -0600

    Adding email and active fields + mysqldump

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_230244) do
+ActiveRecord::Schema.define(version: 2021_02_16_194911) do
 
   create_table "employees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nom"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_02_15_230244) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
+    t.boolean "active"
   end
 
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
