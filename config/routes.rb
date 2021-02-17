4bb7da065304e06b3593d814526ad27ea07512a2
Claude St-Laurent <teambiti@yahoo.com>
2021-02-17 00:30:52 -0500

    relink CSL

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  #get 'page/index'
-  root 'page#index'
-  get  'page/residential'
-  get  'page/commercial'
-  get  'page/quote'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
