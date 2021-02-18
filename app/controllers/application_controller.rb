7d47ee8266efe55c5b42b96864acb8c6d46a2373
ayadisirine <sirine.ayadi93@gmail.com>
2021-02-18 13:23:50 -0600

    admin rails install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    protect_from_forgery with: :exception
+
+    before_action :authenticate_user!
+  
+    protected
+  
+    def after_sign_in_path_for(resource)
+      #current_user.is_a?(Admin) ? admin_tests_path : (stored_location_for(resource) || root_path)
+      
+    end
+
+    def after_sign_out_path_for(scope)
+        # return the path based on scope
+      end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,4 @@
 class Employee < ApplicationRecord
+    has_many :enrollments
+    has
 end
