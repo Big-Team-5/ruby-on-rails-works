1ac21ecc475f3787faf8a904deadde957cc4aba7
Claude St-Laurent <teambiti@yahoo.com>
2021-02-18 16:52:32 -0500

    thu pm CSL

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,21 @@
 Rails.application.routes.draw do
-  resources :stories
   resources :employees
   devise_for :users
   root 'page#index'
   get  'page/residential'
   get  'page/commercial'
   get  'page/quote'
-  get  'sessions/login'
-  get  'users/new'
-  resources :users, only: [:new, :create]
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  get 'welcome', to: 'sessions#welcome'
+  # get  'sessions/login'
+  # get  'users/new'
+  # get  'employees/index'
+  # get  'employees/new'
+  # get  'employees/show'
+  # get  'employees/edit'
+  # get  'registrations/new'
+  # resources :users, only: [:new, :create]
+  # get 'login', to: 'sessions#new'
+  # post 'login', to: 'sessions#create'
+  # get 'welcome', to: 'sessions#welcome'
   # get 'authorized', to: 'sessions#page_requires_login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
